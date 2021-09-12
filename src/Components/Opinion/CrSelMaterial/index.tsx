6cb35bd387b93e29020a602d573b80e8df1ff006
AntonioCastelliRibeiro <antoniocastelliribeiro@hotmail.com>
2021-09-12 11:35:22 -0300

    Ajustes Preload e geral

--- src/Components/Opinion/CrSelMaterial/index.tsx
+++ src/Components/Opinion/CrSelMaterial/index.tsx
@@ -31,42 +31,42 @@ const ContentSec = styled.div`
 // touchAction: 'pan-y'
 
 export default function CrSelMaterial(props: IProps) {
-    const [onIndex, setOnIndex] = useState(0);
-    return (
-        <ContentPrinc fontFamily={props.fontFamily}>
-            <ContentSec>
-                <Carousel
-                    // className="carouselMaterialClass"
-                    autoFocus
-                    onClickItem={() => console.log(onIndex)}
-                    selectedItem={onIndex}
-                    onChange={(index, intem) => setOnIndex(index)}
-                    emulateTouch
-                    showIndicators
-                    // dynamicHeight
-                    showThumbs={false}
-                    showStatus={false}
-                    swipeable
-                    // autoPlay
-                    showArrows
-                    useKeyboardArrows
-                    stopOnHover
-                // centerMode
-                // centerSlidePercentage={90}
-                // infiniteLoop={!isSmall}
-                >
-                    {objData.map((e, count) => {
-                        return (
-                            <div>
-                                <CardCrSel
-                                    fontFamily={props.fontFamily}
-                                    {...e}
-                                    count={count}
-                                />
-                            </div>
-                        )
-                    })}
-                    {/* <div>
+  const [onIndex, setOnIndex] = useState(0);
+  return (
+    <ContentPrinc fontFamily={props.fontFamily}>
+      <ContentSec>
+        <Carousel
+          // className="carouselMaterialClass"
+          // autoFocus
+          onClickItem={() => console.log(onIndex)}
+          selectedItem={onIndex}
+          onChange={(index, intem) => setOnIndex(index)}
+          emulateTouch
+          showIndicators
+          // dynamicHeight
+          showThumbs={false}
+          showStatus={false}
+          swipeable
+          // autoPlay
+          showArrows
+          useKeyboardArrows
+          stopOnHover
+        // centerMode
+        // centerSlidePercentage={90}
+        // infiniteLoop={!isSmall}
+        >
+          {objData.map((e, count) => {
+            return (
+              <div>
+                <CardCrSel
+                  fontFamily={props.fontFamily}
+                  {...e}
+                  count={count}
+                />
+              </div>
+            )
+          })}
+          {/* <div>
             <img
               alt=""
               src="https://images.unsplash.com/photo-1630481786681-a94a4dcb67dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
@@ -80,8 +80,8 @@ export default function CrSelMaterial(props: IProps) {
             />
             <p className="legend">Legend 3</p>
           </div> */}
-                </Carousel>
-            </ContentSec>
-        </ContentPrinc>
-    );
+        </Carousel>
+      </ContentSec>
+    </ContentPrinc>
+  );
 }
